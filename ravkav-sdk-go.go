78dcf20f20b8eaf929a5a548df8183b944493004
Yohanan Baruchel <ybaruchel@gmail.com>
2019-11-02 17:51:46 +0200

    Add comments to main package

--- ravkav-sdk-go.go
+++ ravkav-sdk-go.go
@@ -8,10 +8,14 @@ import (
 
 func main() {}
 
+// NewReader creates a new reader instance used for connecting to
+// the card and transmit commands for getting card's content.
 func NewReader() contracts.Reader {
 	return reader.NewReader()
 }
 
+// NewCard creates a new card instance, it receives a reader instance
+// for flexibility purposes and for tests.
 func NewCard(reader contracts.Reader) contracts.Card {
 	return card.NewByReader(reader)
 }
